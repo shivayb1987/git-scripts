4edf8230df7797bfbbd4f50067c520aca5d5c815
Shiva YB <shivayb1987@gmail.com>
2017-11-02 16:12:34 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -26,7 +26,7 @@ function addChanges {
 			done
 		elif [[ $i =~ $re ]]; 
 		then
-			files+=`git status | grep -A20 "Untracked files:"  | grep -A20 ^$ | grep -v "no changes added to commit" | grep -v ^$ | cut -d":" -f2 | head -$i | tail -1`;
+			files+=`git status | grep -A20 "Untracked files:"  | grep -A20 ^$ | grep -v "no changes added to commit" | grep -v ^$ | cut -d":" -f2 | head -$i | tail -1 `;
 			files+=`echo " "`;
 		fi
 	done
@@ -52,13 +52,17 @@ function untracked {
 }
 
 function add {
-	untracked
-
-	echo -e "\033[01m\033[31mWant to add any file?[y|n] \033[00m"
-	read answ
-	if [[ $answ == "y" ]]; 
+	unstaged=`git status | grep -A20 'Untracked files' | grep -v -e '\.sh' -e '\.txt' -e '\.log' | grep "[\.js\.jsx]$"`
+	if [ ! -z "$unstaged" ];
 	then
-		addChanges
+		untracked
+
+		echo -e "\033[01m\033[31mWant to add any file?[y|n] \033[00m"
+		read answ
+		if [[ $answ == "y" ]]; 
+		then
+			addChanges
+		fi
 	fi
 }
 
@@ -88,12 +92,13 @@ function commitAll {
 	read comment;
 	confirm;
 	git commit -a -m "$comment"
+	confirm
 }
 
 function commitSelected {
 	echo -e "\033[01m\033[31mEnter the number of each file to be committed with a space b/w them ...\033[00m"
 	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++" 
-	git status | grep "modified\|new file\|deleted" | cut -d":" -f2 | grep -n .
+	git status | grep -i "modified\|new file\|deleted\|renamed" | cut -d":" -f2 | grep -n .
 	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++" 
 	read numbers
 	files="";
@@ -107,7 +112,7 @@ function commitSelected {
 			str1=`echo $i |cut -d'-' -f1`
 			str2=`echo $i |cut -d'-' -f2`
 			noOfLines=`expr $str2 - $str1 + 1`;
-			git status | grep "modified\|new file\|deleted" | cut -d":" -f2 | head -$str2 | tail -$noOfLines | while read file
+			git status | grep -i "modified\|new file\|deleted\|renamed" | cut -d":" -f2 | head -$str2 | tail -$noOfLines | while read file
 			do 
 				# echo fiels $files
 				# echo "$file " >> files.txt
@@ -116,7 +121,7 @@ function commitSelected {
 			done
 		elif [[ $i =~ $re ]]; 
 		then
-			files+=`git status | grep "modified\|new file\|deleted" | cut -d":" -f2 | head -$i | tail -1`;
+			files+=`git status | grep -i "modified\|new file\|deleted\|renamed" | cut -d":" -f2 | head -$i | tail -1`;
 			files+=`echo " "`;
 		else 
 		   commitAll
@@ -131,23 +136,37 @@ function commitSelected {
 	else 
 		files+=`cat files.txt`
 	fi
+	echo "$files" | tr -s '[:space:]' " " | tr " " "\n" | while read file
+	do
+		git diff "$file"
+	done
 	echo "Committing $files";
 	echo -e "\033[01m\033[31mPlease enter a comment for the commit ...\033[00m"
 	read comment
+	while [ -z $comment ]
+	do
+		echo -e "\033[01m\033[31mPlease enter a comment for the commit ...\033[00m"
+		read comment
+	done
 	confirm
 	git commit -m "$comment" $files
 	rm files.txt
 }
 
 #BEGIN
-# git checkout build/css/index.css build/css/index.css.map
+git status | grep "behind" | grep "can be fast-forwarded"
+if [[ $? ]]; then
+	git pull
+fi
 
+# confirm
+rm files.txt 2> /dev/null
 git status
 add
 commit
 confirm
 git push
-
+read comment
 
 
 
